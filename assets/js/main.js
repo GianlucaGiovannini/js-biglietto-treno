31d7c8ee049cdf3d5fc717c246e0ebc439c7e1ad
GianlucaGiovannini <gianluca-giova@hotmail.it>
2022-03-24 16:18:55 +0100

    Add template

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,11 @@ Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio,
 L'output del prezzo finale va scritto in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
 */
 
+// chiedere all'utente il suo nome e stampare
+const user_name = prompt("Scrivi il tuo nome")
+document.getElementById("user_name").innerHTML = user_name;
+
+
 //chiedere all'utente il numero di km che vuole percorrere e l'età del che ha
 const km_percorso = Number(prompt("Quanti km vuoi percorrere?"))
 if (isNaN(km_percorso)) {
@@ -35,10 +40,16 @@ const prezzo_minori = prezzo_utente * 0.8;
 const prezzo_over_65 = prezzo_utente * 0.6;
 
 // stampa il prezzo 
-if (età_utente < 18) {
+if (età_utente <= 18) {
     document.getElementById("prezzo_finale").innerHTML = "€ " + prezzo_minori.toFixed(2);
-} else if (età_utente > 65) {
+} else if (età_utente >= 65) {
     document.getElementById("prezzo_finale").innerHTML = "€ " + prezzo_over_65.toFixed(2);
 } else {
     document.getElementById("prezzo_finale").innerHTML = "€ " + prezzo_utente.toFixed(2);
+}
+
+if (condition) {
+    9
+} else {
+
 }
