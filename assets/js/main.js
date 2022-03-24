4f9e5a7c807eff3888790c428296529c1b69e9e6
GianlucaGiovannini <gianluca-giova@hotmail.it>
2022-03-24 16:52:45 +0100

    Code Review

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,26 +17,14 @@ document.getElementById("user_name").innerHTML = user_name;
 
 //chiedere all'utente il numero di km che vuole percorrere e l'età del che ha
 const km_percorso = Number(prompt("Quanti km vuoi percorrere?"))
-if (isNaN(km_percorso)) {
-    alert("Inserisci il numero di km")
-}
-console.log(km_percorso);
-
 const età_utente = Number(prompt("quanti anni hai ?"));
-if (isNaN(età_utente)) {
-    alert("Inserisci un numero")
-}
-console.log(età_utente);
 
-// prezzo al km da percorrere
+
+
+// prezzi
 const prezzo_km = 0.21;
-
-// prezzo utente senza sconti
 const prezzo_utente = prezzo_km * km_percorso;
-
-// calcolo sconti per età
 const prezzo_minori = prezzo_utente * 0.8;
-
 const prezzo_over_65 = prezzo_utente * 0.6;
 
 // stampa il prezzo 
@@ -46,10 +34,4 @@ if (età_utente <= 18) {
     document.getElementById("prezzo_finale").innerHTML = "€ " + prezzo_over_65.toFixed(2);
 } else {
     document.getElementById("prezzo_finale").innerHTML = "€ " + prezzo_utente.toFixed(2);
-}
-
-if (condition) {
-    9
-} else {
-
 }
